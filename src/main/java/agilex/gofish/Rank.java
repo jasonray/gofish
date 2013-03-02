a1fd49e9f89f9847a30baac2cdaefefa94af966c
Jason Ray <jason.ray@agilex.com>
2013-03-02 07:52:34 -0500

    ignore intellij files

---
+++ src/main/java/agilex/gofish/Rank.java
@@ -0,0 +1,11 @@
+package agilex.gofish;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: rayj
+ * Date: 3/2/13
+ * Time: 7:31 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Rank {
+}
